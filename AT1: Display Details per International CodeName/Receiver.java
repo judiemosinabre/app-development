9836532180a38078facc1b60630a4ecfec01c831
Judie Michelle Mosinabre <116361492+judiemosinabre@users.noreply.github.com>
2025-03-31 22:55:15 +0800

    lesson 1 modules

---
+++ AT1: Display Details per International CodeName/Receiver.java
@@ -0,0 +1,32 @@
+package com.example.appdevlesson1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Receiver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_receiver);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent i = getIntent();
+        String output = i.getStringExtra("output");
+
+        TextView txtReceiverOutput = findViewById(R.id.txtReceiverOutput);
+        txtReceiverOutput.setText(output);
+    }
+}
